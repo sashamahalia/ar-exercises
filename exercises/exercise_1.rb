56f9f0c5c94d64416449cf51495ba352ffd4208e
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-26 19:45:05 +0000

    Refactor store name as table element

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-Burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-Richmond = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-Gastown = Store.create(annual_revenue: 190000, mens_apparel: false, womens_apparel: false)
+store = Store.create(name: "burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: 'gastown', annual_revenue: 190000, mens_apparel: false, womens_apparel: false)
 puts Store.count
 
 
