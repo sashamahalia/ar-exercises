2e738e4971085fb09bf30e17a0718eb14c3a35e4
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-26 20:52:26 +0000

    Calculate and print total annual revenue for all stores, average annual revenue and the number of stores with an annual revenue of over 1 million

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "total annual revenue: $#{Store.sum(:annual_revenue)}"
+puts "average annual revenue: $#{Store.average(:annual_revenue)}"
+puts "number of stores with annual revenue over a million: #{Store.where('annual_revenue < 1000000').size}"
